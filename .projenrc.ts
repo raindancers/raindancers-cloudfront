4703be1a8cd1ad7db21c9cddaed1a4469d404fb3
Andrew Frazer <mrpackethead@users.noreply.github.com>
2026-03-22 02:47:04 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Andrew Frazer',
+  authorAddress: 'mrpackethead@users.noreply.github.com',
+  cdkVersion: '2.189.1',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.9.0',
+  name: 'raindancers-cloudfront',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/raindancers/raindancers-cloudfront',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
